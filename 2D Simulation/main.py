dfae4d233bcd2a9c7f6b28ff5996a56661c4bfbe
siddharthgir <43254027+siddharthgir@users.noreply.github.com>
2019-09-10 23:07:52 -0400

    Update main.py

--- 2D Simulation/main.py
+++ 2D Simulation/main.py
@@ -2,23 +2,32 @@ from tkinter import *
 from Location import *
 from Agent import *
 from tokenizer import *
+import vrep
+import time
+import math
+import random
+import csv
 
 def init(data):
+    #Initializing all state variables and adding all relevant agents into the scene
     data.timestep = 0
     data.objects = []
     data.tables =[]
     data.guests = []
+    data.bills = set()
     data.waiters = []
-    newTable(data,(data.width//2,data.height//2),150)
+    newTable(data,"table_1",(data.width//2,data.height//2))
     newWaiterArea(data,"waiter_area",(data.width//2-150,data.height//2),100,100)
-    newChair(data,"seat_1",(data.width//2,data.height//2-130),75)
-    newChair(data,"seat_2",(data.width//2,data.height//2+130),75)
-    newPodium(data,(200,data.height-200//2),400,200)
-    newGuest(data,"C_1",(100,data.height - 100),"group_1")
-    newGuest(data,"C_2",(300,data.height - 100),"group_1")
-    newWaiter(data,"W_1",(200,data.height - 100))
+    newChair(data,"seat_1",(data.width//2,data.height//2-85),(0, 0, math.pi))
+    newChair(data,"seat_2",(data.width//2,data.height//2+85),(0, 0, 0))
+    newPodium(data,"podium",(150,data.height-200//2),300,200)
+    newGuest(data,"C_1","group_1",(50,data.height - 100))
+    newGuest(data,"C_2","group_1",(150,data.height - 100))
+    newWaiter(data,"W_1",(600,data.height - 100))
     data.instructions = tokenize("data.txt")
-    print(data.instructions)
+    data.output = []
+    data.output += [["timestep","objects","agents"]]
+    time.sleep(1)
 
 
 
@@ -27,6 +36,7 @@ def mousePressed(event, data):
 
 
 def keyPressed(event, data):
+    #Move through the timesteps faster by pressing right key
     if(event.keysym == "Right"):
         data.timestep += 1
         for instruction in data.instructions:
@@ -35,35 +45,51 @@ def keyPressed(event, data):
                 instructionHandler(data,instruction)
 
 def instructionHandler(data,instruction):
-    print(instruction)
+    #Switch case to parse through different possible instructions
     for person in data.guests:
         if person.id == instruction[2]:
             entity = person
     if instruction[1] == "go":
         for area in data.objects:
-            if instruction[3] == area.id:
-                place = area
-        entity.move(place)
+            if instruction[3] == area.id: entity.move(area)
     elif instruction[1] == "talk":
         for person in data.guests:
-            if person.id == instruction[3]:
-                entity2 = person
-        entity.talk(entity2)
+            if person.id == instruction[3]: entity.talk(person)
+    elif instruction[1] == "sit":
+        for chair in data.objects:
+            if instruction[3] == chair.id: entity.seat(data,chair)
+    elif instruction[1] == "look":
+        for object in data.guests + data.objects:
+            if instruction[3] == object.id: entity.lookAnimate(object)
+
+
 def timerFired(data):
-    pass
+    #Increments timesteps per second accodring to timerDelay
+    people = dict()
+    for guest in data.guests:
+        people[guest.id] = guest.getJointValues()
+    objects = dict()
+    for object in data.objects:
+        objects[object.id] = object.location
+    data.output += [[data.timestep,objects,people]]
+    data.timestep += 1
+    for instruction in data.instructions:
+        if instruction[0] == str(data.timestep):
+            instructionHandler(data, instruction)
 
 
 def redrawAll(canvas, data):
+    #Drawing all variables
     for table in data.tables:
         table.draw(canvas)
     for object in data.objects:
         if object not in data.tables:
-            #print(object.id)
             object.draw(canvas)
     for guest in data.guests:
         guest.draw(canvas)
     for waiter in data.waiters:
         waiter.draw(canvas)
+
     canvas.create_text(data.width-130,50,text = "Timestep " + str(data.timestep),font = "Arial 20")
 
 
@@ -105,7 +131,25 @@ def run(width=300, height=300):
     keyPressedWrapper(event, canvas, data))
     timerFiredWrapper(canvas, data)
     root.mainloop()
+    with open('person2.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(data.output)
+    csvFile.close()
     print("bye!")
 
+#Program still runs through GUI even if not connected to VREP
 
-run(1000, 1000)
+clientID = vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
+if clientID!=-1:
+    print ('Connected to remote API server')
+    run(800, 800)
+    time.sleep(2)
+    vrep.simxGetPingTime(clientID)
+    vrep.simxFinish(clientID)
+
+else:
+    print ('Failed connecting to remote API server')
+    run(800, 800)
+print ('Program ended')
+
+
